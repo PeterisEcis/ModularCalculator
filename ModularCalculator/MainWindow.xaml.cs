bdbe200ad3302807f389225e232acad00f0269e5
Peteris <peterisecis@gmail.com>
2022-05-03 10:53:28 +0300

    Testing if dll file is working

--- ModularCalculator/MainWindow.xaml.cs
+++ ModularCalculator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CalculatorFunctions;
 
 namespace ModularCalculator
 {
@@ -23,6 +24,11 @@ namespace ModularCalculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Functions.Add(15, 15).ToString());
         }
     }
 }
